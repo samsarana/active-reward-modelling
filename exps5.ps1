2982770e75e8c2558b9012f53df286f2151fc79b
sam-pc <samckclarke@gmail.com>
2019-07-09 17:52:44 +0100

    Script for experiments 6

--- exps5.ps1
+++ exps5.ps1
@@ -3,13 +3,3 @@ python main.py --info=baseline-0-RL --RL_baseline --n_rounds=2
 python main.py --info=baseline-1-random
 
 python main.py --info=active-BALD-10x-T10-p.2 --active_learning=BALD-MC
-
-# python main.py --info=active-BALD-20x --active_learning=BALD-MC --selection_factor=20
-# python main.py --info=active-BALD-50x --active_learning=BALD-MC --selection_factor=50
-# python main.py --info=active-BALD-100x --active_learning=BALD-MC --selection_factor=100
-
-# python main.py --info=active-BALD-T20 --active_learning=BALD-MC --num_MC_samples=20
-# python main.py --info=active-BALD-T50 --active_learning=BALD-MC --num_MC_samples=50
-
-# python main.py --info=active-BALD-p.15 --active_learning=BALD-MC --p_dropout_rm=0.15
-# python main.py --info=active-BALD-p.25 --active_learning=BALD-MC --p_dropout_rm=0.25
