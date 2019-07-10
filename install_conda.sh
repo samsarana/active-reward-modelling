af2170d86cfd57bffaa9019b5e32fd8cd5d34ea2
sam-pc <samckclarke@gmail.com>
2019-07-10 10:12:51 +0100

    Installation files

--- install_conda.sh
+++ install_conda.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /tmp
+cd ~/tmp
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
+conda update -n base -c defaults conda
