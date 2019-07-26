c99f9e0e32362947f69891a7529dc45feed833d7
sam-pc <samckclarke@gmail.com>
2019-07-26 12:59:57 +0100

    Fixed bug in setup

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 conda update -n base -c defaults conda
 conda create -n gym python=3.7
-conda activate gym
-cd ~/barm
-pip install -r requirements.txt
-pip install .
---
+++ setup3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/barm
+pip install -r requirements.txt
+pip install .
