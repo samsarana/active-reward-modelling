e30b763bc7d524d205caf4509669a1745b513212
Sam Clarke <samckclarke@gmail.com>
2019-08-03 17:10:21 +0100

    Added final part

---
+++ experiments/expA1_iv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --info=baseline-2-5ens-reinit_rm+agent --size_rm_ensemble=5      --terminate_once_solved --reinit_rm --reinit_agent --n_agent_train_steps=8000 --n_labels_per_round=1 --n_rounds=100 --n_runs=30
