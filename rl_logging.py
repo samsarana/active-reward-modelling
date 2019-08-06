db29777db6927b77af60998c47cb776e66e450c1
Sam Clarke <samckclarke@gmail.com>
2019-08-06 15:00:23 +0100

    Update tb tags

--- rl_logging.py
+++ rl_logging.py
@@ -13,14 +13,20 @@ def log_agent_step(sa_pair, r_true, rets, reward_stats, reward_model, args):
         rets['ep']['pred_norm'] += r_pred_norm
     return rets
 
-def log_agent_episode(rets, writers, step, i_train_round, args):
+def log_agent_episode(rets, writers, step, i_train_round, args, is_test):
     writer1, writer2 = writers
+    if is_test:
+        tag = '2a.test_ep_return_per_step/round_{}'.format(i_train_round)
+        tag_norm = '2b.test_ep_return_per_step_normalised/round_{}'.format(i_train_round)
+    else: # logging training episode
+        tag = '4a.train_ep_return_per_step/round_{}'.format(i_train_round)
+        tag_norm = '4b.train_ep_return_per_step_normalised/round_{}'.format(i_train_round)
     # interpreting writers: 1 == blue == true!
-    writer1.add_scalar('3a.ep_return_against_step/round_{}'.format(i_train_round), rets['ep']['true'], step+1)
-    writer1.add_scalar('3b.ep_return_against_step_normalised/round_{}'.format(i_train_round), rets['ep']['true_norm'], step+1)
+    writer1.add_scalar(tag, rets['ep']['true'], step)
+    writer1.add_scalar(tag_norm, rets['ep']['true_norm'], step)
     if not args.RL_baseline:
-        writer2.add_scalar('3a.ep_return_against_step/round_{}'.format(i_train_round), rets['ep']['pred'], step+1)
-        writer2.add_scalar('3b.ep_return_against_step_normalised/round_{}'.format(i_train_round), rets['ep']['pred_norm'], step+1)
+        writer2.add_scalar(tag, rets['ep']['pred'], step)
+        writer2.add_scalar(tag_norm, rets['ep']['pred_norm'], step)
     for key, value in rets['ep'].items():
         rets['all'][key].append(value)
         rets['ep'][key] = 0
