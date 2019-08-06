74b859b2372afe50de97b50deedac80f1897db80
Sam Clarke <samckclarke@gmail.com>
2019-08-06 16:07:54 +0100

    Separated setup files so u don't have to install video stuff

--- setup3.sh
+++ setup3.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-cd ~/barm
 pip install -r requirements.txt
 pip install .
-sudo apt-get install python-opengl
-sudo apt-get install ffmpegrm -r logsrm 
-sudo apt install xvfb
---
+++ setup4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install python-opengl
+sudo apt-get install ffmpegrm -r logsrm 
+sudo apt install xvfb
