2a0d3b34fd34a13cc9ed73411a09ef377e48dc37
Sam Clarke <samckclarke@gmail.com>
2019-08-07 17:46:22 +0100

    Separate into 1 more file

--- experiments/expsB1_iii.sh
+++ experiments/expsB1_iii.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 python main.py --env=acrobot_hard --default_settings=openai --n_runs=10 --n_rounds=100 --info=BALD --active_method=BALD --uncert_method=ensemble --size_rm_ensemble=5
-python main.py --env=acrobot_hard --default_settings=openai --n_runs=10 --n_rounds=100 --info=RL --RL_baseline
---
+++ experiments/expsB1_v.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python main.py --env=acrobot_hard --default_settings=acrobot_sam --n_runs=10 --n_rounds=100 --info=RL --RL_baseline
+python main.py --env=acrobot_hard --default_settings=openai --n_runs=10 --n_rounds=100 --info=RL --RL_baseline
