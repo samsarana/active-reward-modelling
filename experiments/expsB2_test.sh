e3283f98228d591a15bf45342dee158aeb7bd6b7
Sam Clarke <samckclarke@gmail.com>
2019-08-07 16:38:42 +0100

    Add experiment

--- experiments/expsB2_test.sh
+++ experiments/expsB2_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python main.py --default_settings=acrobot_sam --n_runs=1 --n_rounds=3 --info=RL_acrobot_test --RL_baseline
 python main.py --default_settings=acrobot_sam --n_runs=1 --n_rounds=3 --info=baseline_acrobot_test
+python main.py --default_settings=acrobot_sam --n_runs=1 --n_rounds=3 --info=active_acrobot_test --active_method=BALD --uncert_method=ensemble --size_rm_ensemble=5
