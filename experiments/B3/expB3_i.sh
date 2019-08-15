18614c55816a03c37335585946cb648d2562cdb9
Sam Clarke <samckclarke@gmail.com>
2019-08-15 12:37:07 +0100

    Add experiments to debug using n_sample_reps

---
+++ experiments/B3/expB3_i.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --env=acrobot_hard --default_settings=acrobot_sam --info=RandAcq-s-rep --n_sample_reps=50
---
+++ experiments/B3/expB3_ii.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --env=acrobot_hard --default_settings=acrobot_sam --info=BALD-s-rep --active_method=BALD --uncert_method=ensemble --size_rm_ensemble=5 --n_sample_reps=50
