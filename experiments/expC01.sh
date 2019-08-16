500c79a6be47835d3317f7cf345cd8a907d02a38
Sam Clarke <samckclarke@gmail.com>
2019-08-16 14:21:24 +0100

    One more set of hyperparams to try

--- experiments/expC01.sh
+++ experiments/expC01.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # cartpole hyperparams for 10 rounds = 80k steps, wiht lr \in {1e-3, 5e-4, 1e-4}
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=1e-3 --default_settings=cartpole --info=RL_fl_cp_1e-3 --no_normalise_rewards
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=5e-4 --default_settings=cartpole --info=RL_fl_cp_5e-4 --no_normalise_rewards
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=1e-4 --default_settings=cartpole --info=RL_fl_cp_1e-4 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=1e-3 --default_settings=cartpole --info=RL_fl_cp_1e-3 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=5e-4 --default_settings=cartpole --info=RL_fl_cp_5e-4 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=1e-4 --default_settings=cartpole --info=RL_fl_cp_1e-4 --no_normalise_rewards
 # openai_default hyperparams for 1 round = 100k steps, with lr \in {1e-3, 5e-4, 1e-4}
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-3 --default_settings=openai --info=RL_fl_opai_1e-3 --no_normalise_rewards
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=5e-4 --default_settings=openai --info=RL_fl_opai_5e-4 --no_normalise_rewards
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-4 --default_settings=openai --info=RL_fl_opai_1e-4 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-3 --default_settings=openai --info=RL_fl_opai_1e-3 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=5e-4 --default_settings=openai --info=RL_fl_opai_5e-4 --no_normalise_rewards
+# python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-4 --default_settings=openai --info=RL_fl_opai_1e-4 --no_normalise_rewards
+# good acrobot defaults for 1 round = 100k steps, with lr \in {1e-3, 5e-4, 1e-4}
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-3 --default_settings=acrobot_sam --info=RL_fl_ac_1e-3 --no_normalise_rewards
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=5e-4 --default_settings=acrobot_sam --info=RL_fl_ac_5e-4 --no_normalise_rewards
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-4 --default_settings=acrobot_sam --info=RL_fl_ac_1e-4 --no_normalise_rewards
