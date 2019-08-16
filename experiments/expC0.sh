42dee270fcf1fec37769ca8b8934f527f4d7e9af
Sam Clarke <samckclarke@gmail.com>
2019-08-16 10:49:17 +0100

    Fix typo

--- experiments/expC0.sh
+++ experiments/expC0.sh
@@ -4,6 +4,6 @@ python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_age
 python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=5e-4 --default_settings=cartpole --info=RL_fl_cp_5e-4
 python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=10 --lr_agent=1e-4 --default_settings=cartpole --info=RL_fl_cp_1e-4
 # openai_default hyperparams for 1 round = 100k steps, with lr \in {1e-3, 5e-4, 1e-4}
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-3 --default_settings=openai_defaults --info=RL_fl_opai_1e-3
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=5e-4 --default_settings=openai_defaults --info=RL_fl_opai_5e-4
-python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-4 --default_settings=openai_defaults --info=RL_fl_opai_1e-4
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-3 --default_settings=openai --info=RL_fl_opai_1e-3
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=5e-4 --default_settings=openai --info=RL_fl_opai_5e-4
+python main.py --env=frozen_lake --RL_baseline --n_runs=1 --n_rounds=1 --lr_agent=1e-4 --default_settings=openai --info=RL_fl_opai_1e-4
