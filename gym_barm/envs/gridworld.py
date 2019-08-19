ce53b2213eebf53ce399357023501fd7ed11f13d
Sam Clarke <samckclarke@gmail.com>
2019-08-19 15:18:29 +0100

    Add support for terminating env if done Add support for determinsitically resetting env

--- gym_barm/envs/gridworld.py
+++ gym_barm/envs/gridworld.py
@@ -22,38 +22,63 @@ class gameOb():
         self.name = name
         
 class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
-    def __init__(self, partial=False, size=5): # sam added defaults
+    def __init__(self, partial=False, size=5, random_resets=True, terminate_ep_if_done=False): # sam added defaults
+        """
+        partial: does agent get partial observations or full state?
+        size: length and width of square grid.
+        random_resets: if True, upon each call to env.reset(),
+                       `gameOb`s will be placed in new random positions
+                       Else, they will be placed in the same,
+                       fixed positions.
+        """
         self.sizeX = size
         self.sizeY = size
         self.actions = 4
         self.objects = []
         self.partial = partial
-        self.reset()
-        plt.imshow(self.state, interpolation="nearest")
-        # Sam added the following two lines
+        self.random_resets = random_resets
+        # Sam added the following three lines
         self.action_space = spaces.Discrete(4) # Discrete(n) is a discrete space in :math:`\{ 0, 1, \\dots, n-1 \}`
         # self.observation_space = spaces.Box(low=0, high=255, shape=(84,84,3)) # Box represents the Cartesian product of n closed intervals
         self.observation_space = spaces.Box(low=0, high=255, shape=(21168,)) # Box represents the Cartesian product of n closed intervals
         # not certain that high is 255 but pretty sure... this comes from scipy.misc.imresize
+        self.determined_locations = {
+            'hero': (0,0),
+            'goal': (0,4),
+            'goal2': (2,3),
+            'goal3': (4,2),
+            'goal4': (4,0),
+            'fire': (1,2),
+            'fire2': (3,1)
+        }
+        self.terminate_ep_if_done = terminate_ep_if_done
+        if not random_resets:
+            assert terminate_ep_if_done,\
+                '''If you want to reset objects deterministically,
+                the game becomes very easy if episodes don't terminate
+                when agent reaches goal/lava'''
+        self.reset()
+        # plt.imshow(self.state, interpolation="nearest")
         
     def reset(self):
         self.objects = []
-        hero = gameOb(self.newPosition(),1,1,2,None,'hero')
+        hero = gameOb(self.newPosition('hero'),1,1,2,None,'hero')
         self.objects.append(hero)
-        bug = gameOb(self.newPosition(),1,1,1,1,'goal')
+        bug = gameOb(self.newPosition('goal'),1,1,1,1,'goal')
         self.objects.append(bug)
-        hole = gameOb(self.newPosition(),1,1,0,-1,'fire')
+        hole = gameOb(self.newPosition('fire'),1,1,0,-1,'fire')
         self.objects.append(hole)
-        bug2 = gameOb(self.newPosition(),1,1,1,1,'goal')
+        bug2 = gameOb(self.newPosition('goal2'),1,1,1,1,'goal2')
         self.objects.append(bug2)
-        hole2 = gameOb(self.newPosition(),1,1,0,-1,'fire')
+        hole2 = gameOb(self.newPosition('fire2'),1,1,0,-1,'fire2')
         self.objects.append(hole2)
-        bug3 = gameOb(self.newPosition(),1,1,1,1,'goal')
+        bug3 = gameOb(self.newPosition('goal3'),1,1,1,1,'goal3')
         self.objects.append(bug3)
-        bug4 = gameOb(self.newPosition(),1,1,1,1,'goal')
+        bug4 = gameOb(self.newPosition('goal4'),1,1,1,1,'goal4')
         self.objects.append(bug4)
         state = self.renderEnv()
         self.state = state
+        self.done = False
         return self._get_obs()
 
     def moveChar(self,direction):
@@ -61,7 +86,7 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
         hero = self.objects[0]
         heroX = hero.x
         heroY = hero.y
-        penalize = 0.
+        penalize = 0. # penalty for taking a step
         if direction == 0 and hero.y >= 1:
             hero.y -= 1
         if direction == 1 and hero.y <= self.sizeY-2:
@@ -75,8 +100,8 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
         self.objects[0] = hero
         return penalize
     
-    def newPosition(self):
-        iterables = [ range(self.sizeX), range(self.sizeY)]
+    def newPosition(self, ob_name=None):
+        iterables = [ range(self.sizeX), range(self.sizeY) ]
         points = []
         for t in itertools.product(*iterables):
             points.append(t)
@@ -86,8 +111,17 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
                 currentPositions.append((objectA.x,objectA.y))
         for pos in currentPositions:
             points.remove(pos)
-        location = np.random.choice(range(len(points)),replace=False)
-        return points[location]
+        if self.random_resets:
+            loc_index = np.random.choice(range(len(points)),replace=False)
+            location = points[loc_index]
+        else: # reset `gameOb`s determinstically
+            try:
+                location = self.determined_locations[ob_name]
+            except KeyError:
+                raise KeyError("{} is not a valid ob_name!".format(ob_name))
+            assert location in points, "You can't place object {} at location {}, it's already occupied!".format(
+                ob_name, location)
+        return location
 
     def checkGoal(self):
         others = []
@@ -96,17 +130,24 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
                 hero = obj
             else:
                 others.append(obj)
-        ended = False
+        done = False
         for other in others:
             if hero.x == other.x and hero.y == other.y:
                 self.objects.remove(other)
-                if other.reward == 1:
-                    self.objects.append(gameOb(self.newPosition(),1,1,1,1,'goal'))
-                else: 
-                    self.objects.append(gameOb(self.newPosition(),1,1,0,-1,'fire'))
-                return other.reward,False
-        if ended == False:
-            return 0.0,False
+                # if other.reward == 1:
+                if self.terminate_ep_if_done:
+                    done = True
+                else: # don't terminate; add the goal or lava back in at a new point
+                    if 'goal' in other.name:
+                        self.objects.append(gameOb(self.newPosition(other.name),
+                        1, 1, 1, 1, other.name))
+                    else:
+                        assert 'fire' in other.name
+                        self.objects.append(gameOb(self.newPosition(other.name),
+                        1, 1, 0, -1, other.name))
+                return other.reward, done
+        # hero moved to unoccupied cell
+        return 0., done
 
     def renderEnv(self):
         #a = np.zeros([self.sizeY,self.sizeX,3])
@@ -114,7 +155,7 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
         a[1:-1,1:-1,:] = 0
         hero = None
         for item in self.objects:
-            a[item.y+1:item.y+item.size+1,item.x+1:item.x+item.size+1,item.channel] = item.intensity
+            a[item.y+1:item.y+item.size+1, item.x+1:item.x+item.size+1, item.channel] = item.intensity
             if item.name == 'hero':
                 hero = item
         if self.partial == True:
@@ -129,10 +170,14 @@ class GridworldEnv(gym.Env): # sam subclassed gameEnv as gym.Env
         return a
 
     def step(self,action):
+        assert not self.done,\
+            '''You are calling 'step()' even though this environment has already returned done = True.
+            You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.'''
         penalty = self.moveChar(action)
         reward, done = self.checkGoal()
         state = self.renderEnv()
         self.state = state
+        self.done = done
         assert reward != None, "Hmm, reward is None. What's going on?"
         return self._get_obs(), (reward + penalty), done, {} # sam added {} - empty info
 
