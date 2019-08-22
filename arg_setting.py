01787d47d9c315887190499674265e25a0c6643d
Sam Clarke <samckclarke@gmail.com>
2019-08-22 19:19:41 +0100

    Correct typo

--- arg_setting.py
+++ arg_setting.py
@@ -184,7 +184,7 @@ def make_arg_changes(args):
         "clip_length ({}) should be a factor of n_agent_steps ({})".format(
         args.clip_length, args.n_agent_steps)
     assert args.n_agent_steps % args.agent_test_frequency == 0,\
-        "agent_test_frequency ({}} should be a factor of n_agent_steps ({})".format(
+        "agent_test_frequency ({}) should be a factor of n_agent_steps ({})".format(
             args.agent_test_frequency, args.n_agent_steps)
     args.n_agent_steps_before_test = args.n_agent_steps // args.agent_test_frequency
 
