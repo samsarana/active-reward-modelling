e4ddf7dc71eab93ee2c5fb20c819e1543d8f8b8e
Sam Clarke <samckclarke@gmail.com>
2019-08-24 15:36:07 +0100

    move LinearSchedule to utils.py

--- q_learning.py
+++ q_learning.py
@@ -223,30 +223,3 @@ def init_agent(args):
     q_target.load_state_dict(q_net.state_dict()) # set params of q_target to be the same
     replay_buffer = ReplayBuffer(args.replay_buffer_size)
     return q_net, q_target, replay_buffer, optimizer_agent
-
-class LinearSchedule(object):
-    """Copy-pasted from:
-       https://github.com/openai/baselines/blob/master/baselines/common/schedules.py
-    """
-    def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
-        """Linear interpolation between initial_p and final_p over
-        schedule_timesteps. After this many timesteps pass final_p is
-        returned.
-        Parameters
-        ----------
-        schedule_timesteps: int
-            Number of timesteps for which to linearly anneal initial_p
-            to final_p
-        initial_p: float
-            initial output value
-        final_p: float
-            final output value
-        """
-        self.schedule_timesteps = schedule_timesteps
-        self.final_p = final_p
-        self.initial_p = initial_p
-
-    def value(self, t):
-        """See Schedule.value"""
-        fraction = min(float(t) / self.schedule_timesteps, 1.0)
-        return self.initial_p + fraction * (self.final_p - self.initial_p)
