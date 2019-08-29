fbb22324119ba1596d6293cfde22292a9a3217f8
Sam Clarke <samckclarke@gmail.com>
2019-08-29 21:44:42 +0100

    collect agent_experience if args.normalise_rewards changed location of one_hot_action

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -5,7 +5,7 @@ from q_learning import *
 from test_policy import *
 from rl_logging import *
 import gym
-from reward_learning_utils import one_hot_action
+from utils import one_hot_action
 
 def do_RL(env, q_net, q_target, optimizer_agent, replay_buffer, epsilon_schedule,
           agent_experience, reward_model, returns_summary,
@@ -30,7 +30,8 @@ def do_RL(env, q_net, q_target, optimizer_agent, replay_buffer, epsilon_schedule
             action_one_hot = one_hot_action(action, env)
         sa_pair = np.append(state, action_one_hot).astype(args.oa_dtype, casting='unsafe') # in case len(state.shape) > 1 (gridworld, atari), np.append will flatten it
         assert (sa_pair == np.append(state, action_one_hot)).all() # check casting done safely. should be redundant since i set oa_dtype based on env, earlier. but you can never be too careful since this would fail silently!
-        if not args.RL_baseline: agent_experience.add(sa_pair, r_true) # include reward in order to later produce synthetic prefs
+        if not args.RL_baseline or args.normalise_rewards:
+            agent_experience.add(sa_pair, r_true) # include reward in order to later produce synthetic prefs
         if args.agent_gets_dones:
             replay_buffer.push(state, action, r_true, next_state, done)
         else:
